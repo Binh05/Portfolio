05ff2bb6058f40e70f07e963e967070b2baf501d
Binh05 <binhdt0452@gmail.com>
2026-04-30 20:29:31 +0700

    Fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://binh05.github.io/Portfolio/",
+  base: "/Portfolio/",
   plugins: [
     tailwindcss(),
     react(),
